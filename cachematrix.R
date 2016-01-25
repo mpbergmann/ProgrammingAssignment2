0affd9ff82bacf6e6f75bf3504ad6d1ecd4e58c4
Marcel Bergmann <mbergman@mbergman-ml1.local>
2016-01-24 23:03:25 -0800

    Functions seem to be working

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions to calculate the inverse of a matrix, and to store that inverse
+## in a cached variable, which can be quickly retrieved should the calculation
+## be attempted again. 
+##
+## Based on the example scripts for vector caching and mean vector calcultions
+## Jan 24, 2016
 
-## Write a short comment describing this function
+## the makeCacheMatrix function defines a cache variable, and stores the inverse## matrix once it has been calculated.
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
+
 
 }
 
 
-## Write a short comment describing this function
+##  The cachSolve function derives the inverse of a matrix, either by pulling
+##  a stored value from the cache, or by calculating it anew if necessary.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
